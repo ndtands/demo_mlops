60b2ac79a96c9e0cd7e8a0816ac13dcae7b819fe
ndtands <ndtan.hcm@gmail.com>
2023-02-24 15:24:47 +0700

    update validation

--- ml_core/test/data-validation.py
+++ ml_core/test/data-validation.py
@@ -1,6 +1,8 @@
 from ml_core import data_validation
 from configs.config import logger
+from decouple import config
 
 if __name__ == '__main__':
-    data_validation.validate_data(project='mlops')
+    PROJECT_NAME = config('PROJECT_NAME')
+    data_validation.validate_data(project=PROJECT_NAME)
     logger.info('Validation Done')
