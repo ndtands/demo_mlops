24954092508fe68a11b6be360c80883d1957e7ad
ndtands <ndtan.hcm@gmail.com>
2023-02-24 11:14:22 +0700

    data extraction

---
+++ ml_core/data_extracion/__init__.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+import pymongo
+import pandas as pd
+import wandb
+
+def fetch_data(collection: pymongo.collection.Collection):
+    list_json = []
+    for i in collection.find():
+        list_json.append(i)
+
+    # Convert jsonl to dataframe
+    df = pd.json_normalize(list_json)
+    return df
+
+def extract_data(project: str, collection: pymongo.collection.Collection):
+    run = wandb.init(project=project, job_type="data-extraction")
+
+    # Create a sample dataset to log as an artifact
+    df = fetch_data(collection=collection)
+
+    # log data artifacts
+    dataset_artifact = wandb.Artifact('raw-dataset', type='dataset')
+    dataset_table = wandb.Table(data=df, columns=df.columns)
+    dataset_artifact.add(dataset_table, 'raw-dataset')
+    run.log_artifact(dataset_artifact)
+
+    wandb.finish()
---
+++ ml_core/test/data-extraction.py
@@ -0,0 +1,18 @@
+from ml_core import data_extraction
+from decouple import config
+from configs.config import logger
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    MONGODB_USER = config('MONGODB_USER')
+    MONGODB_PASSWORD = config('MONGODB_PASSWORD')
+    DATASOURCE = config('DATASOURCE')
+    PROJECT_NAME = config('PROJECT_NAME')
+    MONGODB_URL = f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@cluster0.k0hrmbm.mongodb.net/test'
+    client = MongoClient(MONGODB_URL)
+    db=client[DATASOURCE]
+    collection_post = db.data
+    df = data_extraction.extract_data(project=PROJECT_NAME, collection=collection_post)
+    print(df.shape)
+    logger.info("Data extract Done")
