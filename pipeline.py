4562cce829e3449b2350122e0d08d9f7f524c10d
ndtands <ndtan.hcm@gmail.com>
2023-02-25 21:11:52 +0700

    change URL mongodb

--- pipeline.py
+++ pipeline.py
@@ -10,7 +10,7 @@ if __name__ =='__main__':
     MONGODB_USER = config('MONGODB_USER')
     MONGODB_PASSWORD = config('MONGODB_PASSWORD')
     DATASOURCE = config('DATASOURCE')
-    MONGODB_URL = f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@cluster0.vk4zird.mongodb.net/test'
+    MONGODB_URL = f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@cluster0.k0hrmbm.mongodb.net/test'
     client = MongoClient(MONGODB_URL)
     db=client[DATASOURCE]
     collection_post = db.post
